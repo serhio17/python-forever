74fca32bdf6fc19409ebaf0c9d181191744f0598
Bohdan Danishevsky <bogdan.danishevskyi@sigma.software>
2018-04-18 16:33:21 +0300

    + adds configuration file for flake8 support + configured git ignore + adds review and some tasks for improvement

--- task-01/task-01.py
+++ task-01/task-01.py
@@ -1,6 +1,15 @@
-my_list=[91, 90, 92, -94, 93, -97, 96, 95, 99, 98]
+# WARN: looks good, some fixes and improvements required. Also, I woluld like to introduce flake8 utility
+# (https://habrahabr.ru/company/dataart/blog/318776/) it can be useful to check that PEP8 standard applied for code.
+
+my_list = [91, 90, 92, -94, 93, -97, 96, 95, 99, 98]
 
 # Print only elements which are > 0
+# TODO: please print results with indexes, in this way:
+# Elements which are > 0:
+#     i[0] = 91
+#     i[1] = 90
+#     i[2] = 90
+#     i[4] = 93
 print("Elements which are > 0:")
 for i in my_list:
     if i > 0:
@@ -17,14 +26,16 @@ print()
 # Print elements which have even position in the list
 print("Elements with even position:")
 for i in range(0, len(my_list)):
-    if i %2 == 0:
+    if i % 2 == 0:
         print(my_list[i])
 print()
 
+# TODO: please do not use build in functions, provide you own algorithm to get maximum value
 print("The maximum element in the list is:")
 print(max(my_list))
 print()
 
+# TODO: please do not use build in functions, provide you own algorithm to get minimal value
 print("The minimum element in the list is:")
 print(min(my_list))
 print()
