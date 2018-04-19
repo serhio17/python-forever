fc75cb737d9ea5225f9b507f7402d63b5c929e60
serhio17 <sg.ovsianikov@gmail.com>
2018-04-20 02:50:36 +0300

    Improvements added. Need to add functions.

--- task-01/task-01.py
+++ task-01/task-01.py
@@ -5,17 +5,17 @@
 # for main() function instead of flat code. Transfer your array to each function which will solve sub-task.
 my_list = [91, 90, 92, -94, 93, -97, 96, 95, 99, 98]
 
-# Print only elements which are > 0
-# TODO: please print results with indexes, in this way:
-# Elements which are > 0:
-#     i[0] = 91
-#     i[1] = 90
-#     i[2] = 90
-#     i[4] = 93
+# # Print only elements which are > 0
+# # TODO: please print results with indexes, in this way:
+# # Elements which are > 0:
+# #     i[0] = 91
+# #     i[1] = 90
+# #     i[2] = 90
+# #     i[4] = 93
 print("Elements which are > 0:")
-for i in my_list:
-    if i > 0:
-        print(i)
+for i in range(0, len(my_list)):
+    if my_list[i] > 0:
+        print('i[{0}] = {1}'.format(i, my_list[i]))
 print()
 
 # Print only odd values of elements
@@ -28,17 +28,27 @@ print()
 # TODO: use enumerate instead of range
 # Print elements which have even position in the list
 print("Elements with even position:")
-for i in range(0, len(my_list)):
-    if i % 2 == 0:
-        print(my_list[i])
+for i in enumerate(my_list):
+    if i[1] % 2 == 0:
+        print(i[1])
 print()
 
 # TODO: please do not use build in functions, provide you own algorithm to get maximum value
+# Find a maximum element in the list
+max_element = 0
+for i, j in enumerate(my_list):
+    if j > max_element:
+        max_element = j
+        index = i
 print("The maximum element in the list is:")
-print(max(my_list))
-print()
+print('i[{0}] = {1}'.format(index, my_list[index]))
 
-# TODO: please do not use build in functions, provide you own algorithm to get minimal value
+# # TODO: please do not use build in functions, provide you own algorithm to get minimal value
+# Find a minimum element in the list
+min_element = 0
+for i, j in enumerate(my_list):
+    if j < min_element:
+        min_element = j
+        index = i
 print("The minimum element in the list is:")
-print(min(my_list))
-print()
+print('i[{0}] = {1}'.format(index, my_list[index]))
