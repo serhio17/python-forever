f9af570f8e7ddf0987b01daefea962560253efbe
Bohdan Danishevsky <bogdan.danishevskyi@sigma.software>
2018-04-20 03:35:05 +0300

    some updates

--- task-01/task-01.py
+++ task-01/task-01.py
@@ -1,47 +1,39 @@
 # WARN: looks good, some fixes and improvements required. Also, I woluld like to introduce flake8 utility
 # (https://habrahabr.ru/company/dataart/blog/318776/) it can be useful to check that PEP8 standard applied for code.
 
-# TODO: please divide you code into functions for specific sub-tasks, please use `if __name == "__main__"` and call
 # for main() function instead of flat code. Transfer your array to each function which will solve sub-task.
 my_list = [91, 90, 92, -94, 93, -97, 96, 95, 99, 98]
 
-# # Print only elements which are > 0
-# # TODO: please print results with indexes, in this way:
-# # Elements which are > 0:
-# #     i[0] = 91
-# #     i[1] = 90
-# #     i[2] = 90
-# #     i[4] = 93
 print("Elements which are > 0:")
-for i in range(0, len(my_list)):
-    if my_list[i] > 0:
-        print('i[{0}] = {1}'.format(i, my_list[i]))
+for index, value in enumerate(my_list):
+    if value > 0:
+        print('i[{0}] = {1}'.format(index, value))
 print()
 
 # Print only odd values of elements
 print("Odd elements in the list:")
-for i in my_list:
-    if i % 2 == 1:
-        print(i)
+for index, value in enumerate(my_list):
+    if value % 2 == 1:
+        print('i[{0}] = {1}'.format(index, value))
 print()
 
-# TODO: use enumerate instead of range
 # Print elements which have even position in the list
 print("Elements with even position:")
-for i in enumerate(my_list):
-    if i[1] % 2 == 0:
-        print(i[1])
+for index, value in enumerate(my_list):
+    if index % 2 == 0:
+        print('i[{0}] = {1}'.format(index, value))
 print()
 
-# TODO: please do not use build in functions, provide you own algorithm to get maximum value
 # Find a maximum element in the list
-max_element = 0
-for i, j in enumerate(my_list):
-    if j > max_element:
-        max_element = j
-        index = i
+max_element = None
+max_index = None
+for index, value in enumerate(my_list):
+    if (max_element is None) or (value > max_element):
+        max_element = value
+        max_index = index
+
 print("The maximum element in the list is:")
-print('i[{0}] = {1}'.format(index, my_list[index]))
+print('i[{0}] = {1}'.format(max_index, max_element))
 
 # # TODO: please do not use build in functions, provide you own algorithm to get minimal value
 # Find a minimum element in the list
